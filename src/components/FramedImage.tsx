415640ddd26d76fafc955b4553cece3ed0746274
jbilee <julishahn@gmail.com>
2024-05-03 01:23:24 +0900

    refactor: Add cursor type prop to FramedImage component

--- src/components/FramedImage.tsx
+++ src/components/FramedImage.tsx
@@ -4,22 +4,25 @@ import { IMAGE_SIZES } from "../utils/values";
 type FramedImageProps = {
   url: string;
   size: keyof typeof IMAGE_SIZES;
+  cursorType: string;
 };
 
-export default function FramedImage({ url, size }: FramedImageProps) {
+export default function FramedImage({ url, size, cursorType }: FramedImageProps) {
   return (
     <Wrapper
       src={url}
       $width={IMAGE_SIZES[size].width}
       $height={IMAGE_SIZES[size].height}
       $radius={IMAGE_SIZES[size].radius}
+      $cursorType={cursorType}
     />
   );
 }
 
-const Wrapper = styled.img<{ $width: string; $height: string; $radius: string }>`
+const Wrapper = styled.img<{ $width: string; $height: string; $radius: string; $cursorType: string }>`
   width: ${({ $width }) => $width};
   height: ${({ $height }) => $height};
   border-radius: ${({ $radius }) => $radius};
   object-fit: cover;
+  cursor: ${({ $cursorType }) => $cursorType};
 `;
